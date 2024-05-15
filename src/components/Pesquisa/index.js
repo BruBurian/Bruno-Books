e58403ee35e21ec8086276afb1ea554453243005
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-05-14 21:44:47 -0300

    Add files via upload

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from '../Input'
 import styled from 'styled-components'
-import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import { useEffect, useState } from 'react'
+import { getLivros } from '../../services/livros'
+import { postFavorito } from '../../services/favoritos'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589)
@@ -49,9 +50,22 @@ const Resultado = styled.div`
 
 function Pesquisa() {
 
-    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+    const [ livros, setLivros ] =  useState([])
 
+    useEffect(() => {
+        fetchLivros()
+    }, [])
 
+    async function fetchLivros(){
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    } 
+
+    async function insertFavorito(id){
+        await postFavorito(id)
+        alert(`Livro de id:${id} inserido!`)
+    }
 
     return(
         <PesquisaContainer>
@@ -66,7 +80,7 @@ function Pesquisa() {
                 }}
             />
             { livrosPesquisados.map( livro => (
-                <Resultado>
+                <Resultado onClick={() => insertFavorito(livro.id)}>
                     <p>{ livro.nome }</p>
                     <img src={livro.src}/>
                 </Resultado>
